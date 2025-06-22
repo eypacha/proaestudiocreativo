e99aa2b73c7eff9db83cc5932fbc51d092ac08ca
Ey Pacha! <pachaguionbajo@gmail.com>
2025-06-22 14:58:28 -0300

    refactor(Navbar): adjust layout and add width to sections

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,19 @@
 <template>
-  <nav class="navbar fixed w-full flex items-center justify-center px-8 py-4 ">
-    <ul class="flex gap-8">
+  <nav class="navbar fixed w-full flex items-center justify-center px-8 py-4">
+    <ul class="flex gap-0">
       <li v-for="(section, i) in sections" :key="i">
         <a
-          class="text-lg transition-colors"
+          class="text-lg transition-colors hover:text-lustria text-blue"
           :href="section.href"
+          :style="
+            section.width
+              ? {
+                  width: section.width + 'px',
+                  display: 'inline-block',
+                  textAlign: 'center',
+                }
+              : {}
+          "
         >
           {{ section.label }}
         </a>
@@ -15,9 +24,9 @@
 
 <script setup>
 const sections = [
-  { href: '#sevices', label: 'Servicios' },
-  { href: '#portfolio', label: 'Porfolio' },
-  { href: '#aboutproa', label: 'Sobre PROA' },
-  { href: '#contact', label: 'Contacto' },
+  { href: "#sevices", label: "Servicios", width: 120 },
+  { href: "#portfolio", label: "Porfolio", width: 110 },
+  { href: "#aboutproa", label: "Sobre PROA", width: 130 },
+  { href: "#contact", label: "Contacto", width: 100 },
 ];
 </script>
