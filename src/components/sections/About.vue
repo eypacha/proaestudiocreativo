cf60c7a41820413a2b4eb24537ddf77dd01991f4
Ey Pacha! <pachaguionbajo@gmail.com>
2025-06-22 18:19:58 -0300

    feat(About): add wave background image and enhance section layout for better visual appeal

--- src/components/sections/About.vue
+++ src/components/sections/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="h-[100dvh] p-10 bg-blue text-gray" id="about">
+  <section class="relative h-[100dvh] p-20 px-10 bg-blue text-gray flex flex-col items-center justify-center" id="about">
     <h2 class="text-4xl font-bold mb-2">Sobre PROA</h2>
   </section>
 </template>
@@ -7,3 +7,21 @@
 <script setup>
 // ...existing code...
 </script>
+
+<style scoped>
+
+section::before {
+  content: '';
+  position: absolute;
+  top: -50px;
+  left: 0;
+  width: 100%;
+  height: 60px;
+  background-image: url('/images/wave.png');
+  background-size: 140px;
+  background-position: bottom center;
+  background-repeat: repeat-x;
+  z-index: 2;
+}
+
+</style>
