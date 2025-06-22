d549b188c72a33a8021e39fd349fe36c098d4267
Ey Pacha! <pachaguionbajo@gmail.com>
2025-06-22 16:53:31 -0300

    fix(Hero): update section styles and remove Logo component

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,10 +1,5 @@
 <template>
-  <section class="hero pt-30 py-10 px-4 text-center bg-gray-100 h-[100dvh]">
-   <Logo class="w-100" />
+  <section class="hero pt-30 py-10 px-4 text-center bg-gray h-[140dvh] relative" id="proa">
+    
   </section>
 </template>
-
-<script setup>
-
-import Logo from './Logo.vue';
-</script>
