88f9a1ee443afe015f01f8c5488d624aa178ec07
Ey Pacha! <pachaguionbajo@gmail.com>
2025-06-22 17:53:23 -0300

    feat(About): add About section component and remove unused Section3 component

---
+++ src/components/sections/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="h-[100dvh] p-10 bg-blue text-gray" id="about">
+    <h2 class="text-4xl font-bold mb-2">Sobre PROA</h2>
+  </section>
+</template>
+
+<script setup>
+// ...existing code...
+</script>
