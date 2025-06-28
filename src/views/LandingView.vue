03bcac65ce361ece7c863fe1c677819e60761ac4
Ey Pacha! <pachaguionbajo@gmail.com>
2025-06-28 19:16:50 -0300

    feat(LandingView.vue): add LandingView component with logo display and styling

---
+++ src/views/LandingView.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="p-10 bg-gray text-blue flex items-center justify-center h-[100dvh]">
+        <Logo class="w-100 w-max-[100%]"/>
+    </div>
+</template>
+<script setup>
+
+import Logo from '../components/Logo.vue'
+</script>
