59f37d142a0d66112f9a00f694a4917963f7f80a
Ey Pacha! <pachaguionbajo@gmail.com>
2025-06-29 18:13:11 -0300

    feat(Footer.vue): update footer to include social media icons for enhanced connectivity

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,27 @@
 <template>
-  <footer class="py-6 text-center bg-zinc-900 text-white text-base">
-    <p>&copy; 2025 LoremIpsumSups. {{ $t('footer.rights') }}</p>
+  <footer class="waves relative py-6 text-center bg-blue text-white text-base flex items-center justify-center gap-17">
+    <div class="bg-lila rounded-full">
+      <IconWhatsapp color="#dbdddd"/>
+    </div>
+    <div class="bg-lila rounded-full">
+      <IconInstagram color="#dbdddd"/>
+    </div>
+    <div class="bg-lila rounded-full">
+      <IconEnvelope color="#dbdddd"/>
+    </div>
+    <div class="bg-lila rounded-full">
+      <IconFacebook color="#dbdddd"/>
+    </div>
+    <div class="bg-lila rounded-full">
+      <IconLinkedin color="#dbdddd"/>
+    </div>
   </footer>
 </template>
 
 <script setup>
-// Footer component logic (if needed)
+import IconWhatsapp from "@/components/icons/iconWhatsapp.vue";
+import IconInstagram from "@/components/icons/iconInstagram.vue";
+import IconEnvelope from "@/components/icons/iconEnvelope.vue";
+import IconFacebook from "@/components/icons/iconFacebook.vue";
+import IconLinkedin from "@/components/icons/iconLinkedin.vue";
 </script>
-
-<!-- Tailwind CSS used, no scoped CSS -->
