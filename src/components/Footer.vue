49d5e768d33e97c178d0d848f27d83bd10fa97be
Ey Pacha! <pachaguionbajo@gmail.com>
2025-07-02 20:27:57 -0300

    refactor(Footer.vue): simplify footer structure by using v-for for icon rendering

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,11 @@
 <template>
-  <footer class="waves relative pt-3 pb-20 text-center bg-blue text-white text-base flex items-center justify-center gap-17">
-    <div class="bg-lila rounded-full">
-      <IconWhatsapp color="#dbdddd" class="w-15 h-15"/>
-    </div>
-    <div class="bg-lila rounded-full">
-      <IconInstagram color="#dbdddd" class="w-15 h-15"/>
-    </div>
-    <div class="bg-lila rounded-full">
-      <IconEnvelope color="#dbdddd" class="w-15 h-15"/>
-    </div>
-    <div class="bg-lila rounded-full">
-      <IconFacebook color="#dbdddd" class="w-15 h-15"/>
-    </div>
-    <div class="bg-lila rounded-full">
-      <IconLinkedin color="#dbdddd" class="w-15 h-15"/>
-    </div>
-  </footer>
+  <div class="waves w-full">
+    <footer class="relative pt-15 pb-20 text-center text-white text-base flex items-center justify-center gap-17">
+      <div v-for="(icon, i) in icons" :key="i" class="bg-lila rounded-full cursor-pointer">
+        <component :is="icon.component" color="#dbdddd" class="w-15 h-15" />
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script setup>
@@ -24,4 +14,12 @@ import IconInstagram from "@/components/icons/iconInstagram.vue";
 import IconEnvelope from "@/components/icons/iconEnvelope.vue";
 import IconFacebook from "@/components/icons/iconFacebook.vue";
 import IconLinkedin from "@/components/icons/iconLinkedin.vue";
+
+const icons = [
+  { component: IconWhatsapp },
+  { component: IconInstagram },
+  { component: IconEnvelope },
+  { component: IconFacebook },
+  { component: IconLinkedin },
+];
 </script>
