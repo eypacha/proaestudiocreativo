643a6c33b267d632b703f4fbc4b8a0481d2d841d
Ey Pacha! <pachaguionbajo@gmail.com>
2025-07-26 17:00:59 -0300

    fix(Testimonial.vue): refactor iconLike component to use dynamic heartbeat animation class

--- src/components/Testimonial.vue
+++ src/components/Testimonial.vue
@@ -1,15 +1,19 @@
 <template>
     <div class="text-center text-xl flex flex-col items-center gap-3">
-        <iconLike class="mb-3 w-15 animate-heartbeat"/>
+        <iconLike ref="iconRef" :class="['mb-3 w-15', heartbeatClass]"/>
         <div class="text-[18px]">"{{ text }}"</div>
         <div class="text-[16px] text-lustria">{{ name }}</div>
     </div>
 </template>
 
 <script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue';
 import iconLike from './icons/iconLike.vue';
 
-defineProps({
+const iconRef = ref(null);
+const heartbeatClass = ref('');
+
+const props = defineProps({
   text: {
     type: String,
     required: true,
@@ -19,11 +23,24 @@ defineProps({
     required: true,
   },
 });
+
+let intervalId;
+onMounted(() => {
+  intervalId = setInterval(() => {
+    heartbeatClass.value = 'animate-heartbeat';
+    setTimeout(() => {
+      heartbeatClass.value = '';
+    }, 1000);
+  }, 3000);
+});
+onBeforeUnmount(() => {
+  clearInterval(intervalId);
+});
 </script>
 
 <style scoped>
 .animate-heartbeat {
-  animation: heartbeat 1s ;
+  animation: heartbeat 1s forwards;
 }
 @keyframes heartbeat {
   0%, 100% {
