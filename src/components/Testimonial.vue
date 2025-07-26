2c36f84dcb15909dd05796466e549d7b8cf46755
Ey Pacha! <pachaguionbajo@gmail.com>
2025-07-26 17:00:45 -0300

    fix(Testimonial.vue): add heartbeat animation to icon for enhanced visual appeal

--- src/components/Testimonial.vue
+++ src/components/Testimonial.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="text-center text-xl flex flex-col items-center gap-3">
-        <iconLike class="mb-3 w-15"/>
+        <iconLike class="mb-3 w-15 animate-heartbeat"/>
         <div class="text-[18px]">"{{ text }}"</div>
         <div class="text-[16px] text-lustria">{{ name }}</div>
     </div>
@@ -20,3 +20,32 @@ defineProps({
   },
 });
 </script>
+
+<style scoped>
+.animate-heartbeat {
+  animation: heartbeat 1s ;
+}
+@keyframes heartbeat {
+  0%, 100% {
+    transform: scale(1);
+  }
+  10% {
+    transform: scale(1.1);
+  }
+  20% {
+    transform: scale(1.2);
+  }
+  30% {
+    transform: scale(1.1);
+  }
+  40% {
+    transform: scale(1.2);
+  }
+  50% {
+    transform: scale(1.1);
+  }
+  60% {
+    transform: scale(1);
+  }
+}
+</style>
