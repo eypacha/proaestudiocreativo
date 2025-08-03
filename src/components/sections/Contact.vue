53715ac9a0af3f61d8c00d7c6dc7c18fab9d34ad
Ey Pacha! <pachaguionbajo@gmail.com>
2025-08-03 14:28:07 -0300

    fix(Contact.vue): update heading size and text for improved clarity and responsiveness

--- src/components/sections/Contact.vue
+++ src/components/sections/Contact.vue
@@ -3,10 +3,10 @@
     class="relative w-full pt-30 pb-20 px-10 bg-gray text-blue flex flex-col items-center justify-center flex-1"
     id="contact"
   >
-    <h2 class="text-3xl md:text-[40px] font-bold leading-8 mb-2 text-center">¿Tenés un proyecto en mente?</h2>
-    <h3 class="text-xl md:text-2xl text-lustria leading-6 mb-10 text-center">Escribinos para ver cómo podemos ayudarte</h3>
+    <h2 class="text-3xl md:text-[40px] font-bold leading-8 mb-2 md:mb-5 text-center">¿Tenés un proyecto en mente?</h2>
+    <h3 class="text-xl md:text-3xl text-lustria leading-6 mb-10 text-center">Escribinos y te ayudamos</h3>
 
-    <form class="w-full max-w-2xl flex flex-col gap-6">
+    <form class="w-full max-w-2xl flex flex-col gap-6 md:px-10 mt-4 md:mt-6">
       <div class="flex gap-4">
         <div class="w-1/2">
           <FloatLabel>
