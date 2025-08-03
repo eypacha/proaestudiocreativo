fb6aafb32cf7182d3e65614490b44c8188a2c834
Ey Pacha! <pachaguionbajo@gmail.com>
2025-08-03 13:16:41 -0300

    fix(Testimonials.vue): update layout classes for improved responsiveness and replace duplicate testimonial entry

--- src/components/sections/Testimonials.vue
+++ src/components/sections/Testimonials.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="relative pb-20 pt-40 px-7 md:px-10 bg-gray text-blue flex flex-col items-center justify-center" id="testimonials">
     <h2 class="uppercase text-center">
-      <span class="font-bold text-2xl md:text-[40px]">Historias que encontraro rumbo y dirección,<br/></span>
-      <span class="text-lustria text-2xl md:text-4xl">que nos recuerdan por qué hacemos lo que hacemos</span>
+      <span class="font-bold text-2xl lg:text-[40px]">Historias que encontraro rumbo y dirección,<br/></span>
+      <span class="text-lustria text-2xl lg:text-4xl">que nos recuerdan por qué hacemos lo que hacemos</span>
     </h2>
-    <div class="w-full mt-20 flex flex-col md:flex-row gap-20">
+    <div class="w-full mt-20 flex flex-col lg:flex-row gap-20">
       <div class="flex-1">
         <Testimonial text="La asesoría me sirvió muchísimo. No había pensado mi esquema de trabajo en redes de una forma tan clara y me di cuenta de que me faltaban herramientas para saber qué decir, qué es realmente útil y qué no. Me fui muy contenta y súper agradecida, además el encuentro duró más de lo previsto y sentí que se dedicaron a fondo." name="Karen, Tinto Rebelde"/>
       </div>
@@ -12,7 +12,10 @@
         <Testimonial text="La asesoría me sirvió muchísimo. No había pensado mi esquema de trabajo en redes de una forma tan clara y me di cuenta de que me faltaban herramientas para saber qué decir, qué es realmente útil y qué no. Me fui muy contenta y súper agradecida, además el encuentro duró más de lo previsto y sentí que se dedicaron a fondo." name="Karen, Tinto Rebelde"/>
       </div>
       <div class="flex-1">
-        <Testimonial text="La asesoría me sirvió muchísimo. No había pensado mi esquema de trabajo en redes de una forma tan clara y me di cuenta de que me faltaban herramientas para saber qué decir, qué es realmente útil y qué no. Me fui muy contenta y súper agradecida, además el encuentro duró más de lo previsto y sentí que se dedicaron a fondo." name="Karen, Tinto Rebelde"/>
+        <Testimonial 
+          text="Confiar en PROA para crear mi web fue la mejor decisión. Supieron captar mi esencia y plasmarla en cada detalle del sitio, acompañándome en todo el proceso con ideas creativas y soluciones prácticas. Ahora tengo una página que realmente me representa y conecta con mi público."
+          name="Ey Pacha!"
+        />
       </div>
     </div>
     
